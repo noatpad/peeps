c7948621e55f20a7fef4b7ac98eb72733cd9e0d3
Daniel Hernandez <tamdannyhdz@gmail.com>
2021-01-22 23:53:54 -0600

    Prevent body scroll on modal

--- components/Modal/index.js
+++ components/Modal/index.js
@@ -1,26 +1,34 @@
 import React from 'react';
 import ReactModal from 'react-modal';
 
-const Modal = ({ show, close, children }) => (
-  <ReactModal
-    isOpen={show}
-    overlayClassName={{
-      base: "ReactModal__Overlay fixed top-0 left-0 bottom-0 right-0 flex justify-center items-center bg-black bg-opacity-70 z-30",
-      afterOpen: "ReactModal__Overlay--after-open",
-      beforeClose: "ReactModal__Overlay--before-close"
-    }}
-    className={{
-      base: "ReactModal__Content px-8 py-6 rounded-xl bg-white",
-      afterOpen: "ReactModal__Content--after-open",
-      beforeClose: "ReactModal__Content--before-close"
-    }}
-    shouldCloseOnEsc={true}
-    shouldCloseOnOverlayClick={true}
-    onRequestClose={close}
-    closeTimeoutMS={250}
-  >
-    {children}
-  </ReactModal>
-)
+const Modal = ({ show, close, width, children }) => {
+  const onAfterOpen = () => { document.body.style.overflow = 'hidden' }
+
+  const onAfterClose = () => { document.body.removeAttribute('style') }
+
+  return (
+    <ReactModal
+      isOpen={show}
+      overlayClassName={{
+        base: "ReactModal__Overlay fixed top-0 left-0 bottom-0 right-0 flex justify-center items-center bg-black bg-opacity-70 z-30",
+        afterOpen: "ReactModal__Overlay--after-open",
+        beforeClose: "ReactModal__Overlay--before-close"
+      }}
+      className={{
+        base: `ReactModal__Content ${width} px-8 py-6 rounded-xl bg-white`,
+        afterOpen: "ReactModal__Content--after-open",
+        beforeClose: "ReactModal__Content--before-close"
+      }}
+      shouldCloseOnEsc={true}
+      shouldCloseOnOverlayClick={true}
+      onAfterOpen={onAfterOpen}
+      onRequestClose={close}
+      onAfterClose={onAfterClose}
+      closeTimeoutMS={250}
+    >
+      {children}
+    </ReactModal>
+  )
+}
 
 export default Modal;
