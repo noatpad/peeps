3d185067f0263d126d0dc11449a268c89c787f68
Daniel Hernandez <tamdannyhdz@gmail.com>
2021-01-23 23:56:19 -0600

    Make login a bit easier on dev

--- pages/hello.js
+++ pages/hello.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { startAuth } from '@web-utils/api';
 
+import Title from '@components/Title';
+import Button from '@components/Button';
+
 // TODO: Implement and style landing page
-const Hello = () => {
-  return (
-    <div>
-      <h1>Say hi~</h1>
-      <button onClick={startAuth}>Authenticate?</button>
-    </div>
-  )
-}
+const Hello = () => (
+  <main className="flex flex-col justify-center items-center h-screen w-full text-center">
+    <Title/>
+    <Button run={startAuth} primary>Authenticate?</Button>
+  </main>
+)
 
 export default Hello;
