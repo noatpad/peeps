491f26dc4a4febf1d37df62c27c794108bc3c4fa
Daniel Hernandez <tamdannyhdz@gmail.com>
2021-02-01 12:57:24 -0600

    Invalidate token on logout

--- pages/api/auth/logout.js
+++ pages/api/auth/logout.js
@@ -1,13 +1,15 @@
 import nc from 'next-connect';
 import morgan from 'morgan';
-import { expireCookie } from '@api-utils/cookies';
-import { errorStatus } from '@api-utils/twitter';
+import { expireCookie, getToken } from '@api-utils/cookies';
+import { errorStatus, post } from '@api-utils/twitter';
 
 
 const logout = nc()
   .use(morgan('dev'))
   .post(async (req, res) => {
+    const [token, secret] = getToken(req.cookies);
     try {
+      await post(token, secret, '/oauth/invalidate_token');
       res.setHeader('Set-Cookie', expireCookie());
       console.log('Logged off!');
       return res.status(200).send('Logged off!');
